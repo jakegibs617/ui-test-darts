42544bdd1f3e0036b40df26b2cfea07a8b36095e
Jacob <jakegibs617@gmail.com>
2015-11-06 11:56:56 -0500

    a player's total score is calculated, but there is duplication

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ var leaderBoard = function() {
     for (var i = 0; i < array.length; i++) { 
       if ( i < 1 ){
         array[0].rank = 1;
-        rankedArray.push(array[1]);
+        rankedArray.push(array[0]);
       } else {
         if (array[i].score == rankedArray[i - 1].score) {
           array[i].rank = rankedArray[i -1 ].rank;
@@ -89,8 +89,18 @@ if (nameScoreObject.name.length > 0 && nameScoreObject.score > 0) {
 
   // iterate through rankingBoard slots names and if exist add 
   // the scores
-  
+  for (var i = rankingBoard.length - 1; i >= 0; i--) {
+    
+    if (rankingBoard[i].name == nameScoreObject.name){
+      rankingBoard[i].score = parseInt(rankingBoard[i].score, 10) + parseInt(nameScoreObject.score, 10);
+      //adding successfully
+    } else{
+    };
+    
+  };
+//here we are having duplicates if their name already existed...
   rankingBoard.push(nameScoreObject);
+  
 
 console.log(rankingBoard)
 }
